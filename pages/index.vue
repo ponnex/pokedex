91387471908ff2a9c784b03fbe489964b5496c05
ponnex <ponnex@gmail.com>
2021-01-26 02:41:09 +0800

    Added responsive listing Added sticky search

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,33 @@
 <template>
-	<div class="space-y-4">
-		<div class="grid grid-cols-12">
-			<h1 class="col-span-11 text-red-400 text-3xl font-semibold">Pokédex</h1>
-			<svg
-				fill="none"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-				class="w-6 h-6 col-span-1 justify-self-end self-center"
-				@click="onChangeTheme()"
+	<div class="block max-w-screen-2xl mx-auto">
+		<div class="space-y-4 sticky top-0 z-10 bg-white dark:bg-black pt-4 pb-2">
+			<div class="grid grid-cols-12">
+				<h1 class="col-span-11 text-red-400 text-3xl font-semibold">Pokédex</h1>
+				<svg
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					class="w-6 h-6 col-span-1 justify-self-end self-center"
+					@click="onChangeTheme()"
+				>
+					<path v-if="$colorMode.preference == 'light'" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path>
+					<path v-if="$colorMode.preference == 'dark'" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path>
+				</svg>
+			</div>
+			<input
+				id="search-pokemon"
+				type="search"
+				name="search-pokemon"
+				placeholder="Search for a Pokémon"
+				class="block font-medium min-w-full text-gray-500 dark:text-black placeholder-gray-500 dark:placeholder-gray-900 bg-gray-200 dark:bg-gray-50 rounded-2xl py-1 px-3 mr-2"
+				@input="event => onSearch(event)"
 			>
-				<path v-if="$colorMode.preference == 'light'" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path>
-				<path v-if="$colorMode.preference == 'dark'" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path>
-			</svg>
 		</div>
-		<input
-			id="search-pokemon"
-			type="search"
-			name="search-pokemon"
-			placeholder="Search for a Pokémon"
-			class="block font-medium min-w-full text-gray-500 dark:text-black placeholder-gray-500 dark:placeholder-gray-900 bg-gray-200 dark:bg-gray-50 rounded-2xl py-1 px-3 mr-2"
-			@input="event => onSearch(event)"
-		>
-		<span class="block font-medium text-sm text-gray-500 dark:text-white">The Pokédex contains detailed stats for every creature from the Pokémon games.</span>
-		<div class="block space-y-3">
+		<span class="block pb-4 md:pt-4 md:pb-4 lg:pt-4 lg:pb-8 font-medium text-sm text-gray-500 dark:text-white">The Pokédex contains detailed stats for every creature from the Pokémon games.</span>
+		<div class="block space-y-3 sm:space-y-0 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 sm:gap-4 sm:items-center">
 			<pokemon-card
 				v-for="(pokemon, pokemonIdx) in pokemonList"
 				:key="pokemonIdx"
